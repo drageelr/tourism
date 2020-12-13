5fa858093efc7492d2830a16ad25e724b7df8fa9
Hammad Nasir <hammadn99@gmail.com>
2020-12-13 23:19:51 +0500

    api route change

--- client/src/components/auth/api.js
+++ client/src/components/auth/api.js
@@ -1,4 +1,4 @@
-const base_url = "http://localhost:8080/api/";
+const base_url = "http://139.59.110.100/api/";
 
 /**
 API Caller helper to refactor common API code that requires bearer tokens (all http requests have POST method)
